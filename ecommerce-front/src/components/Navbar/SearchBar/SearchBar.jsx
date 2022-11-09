5c626b9c0afc0fa29eb7fcb47493e883368a30f3
sergio <sheko3857@gmail.com>
2022-11-09 00:31:36 -0800

    se volvio hacer responsivo

--- ecommerce-front/src/components/Navbar/SearchBar/SearchBar.jsx
+++ ecommerce-front/src/components/Navbar/SearchBar/SearchBar.jsx
@@ -7,12 +7,25 @@ import SearchOutlinedIcon from '@mui/icons-material/SearchOutlined';
 
 
 const Search = styled("div")(({theme})=>({
+    display:"none",
     backgroundColor:"#fafafa",
     padding:"0 20px ",
     borderRadius: theme.shape.borderRadius,
     width: "80%",
     height: "30%",
-    marginTop:".5rem"
+    marginTop:".5rem",
+    [theme.breakpoints.up("sm")]:{
+        display:"flex"
+       }
+    
+  
+}))
+
+const SearchIcono = styled(SearchOutlinedIcon)(({theme})=>({
+    display:"none",
+    [theme.breakpoints.up("sm")]:{
+        display:"flex"
+       }
     
   
 }))
@@ -22,7 +35,7 @@ const SearchBar = () =>{
         <>
             <Search><InputBase placeholder="Search..."/></Search>
             <IconButton>
-            <SearchOutlinedIcon color="primary" sx={{paddingLeft:"rem",marginTop:".5rem"}}  />
+            <SearchIcono color="primary" sx={{paddingLeft:"rem",marginTop:".5rem"}}  />
             </IconButton>
             {/* <Button sx={{marginLeft:".5rem"}} variant="outlined" color="primary" startIcon={
             <SearchOutlinedIcon sx={{paddingLeft:".5rem"}}  />
