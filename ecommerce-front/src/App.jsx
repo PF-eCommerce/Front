c9fff3fb8c3427f4a7787a3655731762ef189b73
alegfink <alegfink@gmail.com>
2022-11-11 01:39:43 -0300

    creacion LandingPage, falta sacar bordes

--- ecommerce-front/src/App.jsx
+++ ecommerce-front/src/App.jsx
@@ -1,15 +1,22 @@
 import {Box} from "@mui/material"
 import Navbar from "./components/Navbar/Navbar";
-
+// import Cards from "./components/Cards/Cards";
+import {BrowserRouter, Route, Routes } from 'react-router-dom';
+import Landing from "./pages/Landing/Landing";
 
 function App() {
   return (
     <div>
+      <BrowserRouter>
       <Box>
+      
         <Navbar/>
+        <Landing/>
+        
         
       
       </Box>
+      </BrowserRouter>
     </div>
   );
 }
