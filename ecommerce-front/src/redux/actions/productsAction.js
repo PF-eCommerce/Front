2579294011a26f4f504f18e0da8fd11be4dfdcac
Angel Scutari <antonioscutari2@hotmail.com>
2022-11-11 11:12:11 -0300

    Paginado redux

--- ecommerce-front/src/redux/actions/productsAction.js
+++ ecommerce-front/src/redux/actions/productsAction.js
@@ -1,11 +1,7 @@
-import axios from "axios"
+import axios from "axios";
 
 export const GET_ALL_PRODUCTS = "GET_ALL_PRODUCTS";
-
-
-
-
-
+export const USE_PAGINATION = "USE_PAGINATION";
 
 axios.defaults.baseURL = "http://localhost:3001";
 
@@ -20,6 +16,19 @@ export const getAllProducts = () => {
       });
     } catch (error) {
       console.log(error);
+    }
+  };
+};
+export const getOtherPages = (num) => {
+  return async (dispatch) => {
+    try {
+      const paginatedProducts = await axios.get(`/products?page=${num}`);
+      return dispatch({
+        type: USE_PAGINATION,
+        payload: paginatedProducts.data,
+      });
+    } catch (err) {
+      console.log(err);
     }
   };
 };
