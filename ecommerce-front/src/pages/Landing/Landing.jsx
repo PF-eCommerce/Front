070d5b67a04d9183771bea179efa6bb7aa703a6c
alegfink <alegfink@gmail.com>
2022-11-11 14:46:55 -0300

    Landing Hover terminado y componentes botones

--- ecommerce-front/src/pages/Landing/Landing.jsx
+++ ecommerce-front/src/pages/Landing/Landing.jsx
@@ -3,6 +3,83 @@ import { styled } from "@mui/material/styles";
 import Box from "@mui/material/Box";
 import Paper from "@mui/material/Paper";
 import Grid from "@mui/material/Unstable_Grid2";
+import { Button, Typography } from "@mui/material";
+
+const BootstrapButton = styled(Button)({
+  boxShadow: 'none',
+  textTransform: 'none',
+  fontSize: 20,
+  padding: '6px 12px',
+  border: '1px solid',
+  lineHeight: 1.5,
+  backgroundColor: 'aquamarine',
+  borderColor: '#0063cc',
+  fontFamily: [
+    '-apple-system',
+    'BlinkMacSystemFont',
+    '"Segoe UI"',
+    'Roboto',
+    '"Helvetica Neue"',
+    'Arial',
+    'sans-serif',
+    '"Apple Color Emoji"',
+    '"Segoe UI Emoji"',
+    '"Segoe UI Symbol"',
+  ].join(','),
+  '&:hover': {
+    backgroundColor: '#0069d9',
+    borderColor: '#0062cc',
+    boxShadow: 'none',
+    opacity: '0.5',
+    transform: 'scale(1)'
+  },
+  '&:active': {
+    boxShadow: 'none',
+    backgroundColor: '#0062cc',
+    borderColor: '#005cbf',
+  },
+  '&:focus': {
+    boxShadow: '0 0 0 0.2rem rgba(0,123,255,.5)',
+  },
+});
+
+const ImageButton = styled(Button)({
+  boxShadow: 'none',
+  textTransform: 'none',
+  // fontSize: 20,
+  padding: '6px 12px',
+  // border: '1px solid',
+  lineHeight: 1.5,
+  backgroundColor: 'aquamarine',
+  borderColor: '#0063cc',
+  fontFamily: [
+    '-apple-system',
+    'BlinkMacSystemFont',
+    '"Segoe UI"',
+    'Roboto',
+    '"Helvetica Neue"',
+    'Arial',
+    'sans-serif',
+    '"Apple Color Emoji"',
+    '"Segoe UI Emoji"',
+    '"Segoe UI Symbol"',
+  ].join(','),
+  '&:hover': {
+    backgroundColor: '#0069d9',
+    borderColor: '#0062cc',
+    boxShadow: 'none',
+    opacity: '0.7',
+    transform: 'scale(1.05)'
+  },
+  '&:active': {
+    boxShadow: 'none',
+    backgroundColor: '#0062cc',
+    borderColor: '#005cbf',
+  },
+  '&:focus': {
+    boxShadow: '0 0 0 0.2rem rgba(0,123,255,.5)',
+  },
+});
 
 const Landing = () => {
   // const useStyles = styled(theme => ({
@@ -27,7 +104,15 @@ const Landing = () => {
     background: "none",
     border: "none",
     boxShadow: 'none',
+    
   }));
+
+  const CustomItem = styled(Paper, {})({
+    ".MuiButton-root": {color: '#FF0000'}
+    
+  });
+  
+  
 
   const Item1 = styled(Paper)(({ theme }) => ({
     //   backgroundColor: theme.palette.mode === 'dark' ? '#1A2027' : '#fff',
@@ -48,9 +133,12 @@ const Landing = () => {
             backgroundImage: `url(https://images.squarespace-cdn.com/content/v1/5f173b6f507f722ff8fd049d/1616490580704-S2KYVQT66SCCMQHWB26F/Tendencias+en+ropa+para+mujer+joven+verano+2021.jpg})`,
             height: "650px",
             marginTop: 20,
+            marginBottom: 5,
             backgroundSize: "cover",
             backgroundRepeat: "no-repeat",
             backgroundPosition: "center center",
+            
+
             //  backgroundAttachment: 'fixed'
           }}
         >
@@ -146,7 +234,7 @@ const Landing = () => {
             20.22
           </Item>
           </Grid>
-          <Grid xs={3} xsOffset={4.9}
+          <Grid xs={12}
           style={{
             marginBottom: "45px",
             marginTop: '-30px'
@@ -154,18 +242,23 @@ const Landing = () => {
           >
           <Item
             style={{
-              fontSize: "30px",
-              fontFamily: "monospace",
+              // fontSize: "30px",
+              // fontFamily: "monospace",
               display: "flex",
               justifyContent: "center",
-              width: "250px",
-              border: 'solid white 2px',
-              backgroundColor: 'aquamarine',
-              color: 'black',
+              // width: "250px",
+              // border: 'solid white 2px',
+              // background: 'none',
+              // // color: 'black',
+              // boxShadow:'none',
+
               
             }}
+            
           >
-            IR A SELECCIÓN
+            <BootstrapButton
+            >IR A SELECCIÓN</BootstrapButton>
+            
           </Item>
           </Grid>
           
@@ -175,34 +268,62 @@ const Landing = () => {
           <Grid
             xs={5.8}
             style={{
+              // backgroundImage: `url(https://img.freepik.com/fotos-premium/chica-adolescente-elegante-ropa-casual-camina-confianza-calle-verano-dia-mundial-turismo_381014-484.jpg)`,
+              // height: "400px",
+              marginLeft: 10,
+              // borderRadius: "25px",
+              // backgroundSize: "cover",
+              // backgroundRepeat: "no-repeat",
+              // backgroundPosition: "center center",
+              // '&:hover': {
+              
+              //   opacity: '0.5',
+              //   transform: 'scale(1.05)'
+              // },
+              //  backgroundAttachment: 'fixed'
+            }}
+          >
+            <ImageButton
+            style={{
               backgroundImage: `url(https://img.freepik.com/fotos-premium/chica-adolescente-elegante-ropa-casual-camina-confianza-calle-verano-dia-mundial-turismo_381014-484.jpg)`,
               height: "400px",
-              margin: 10,
+              width: '100%',
               borderRadius: "25px",
               backgroundSize: "cover",
               backgroundRepeat: "no-repeat",
               backgroundPosition: "center center",
-              //  backgroundAttachment: 'fixed'
             }}
-          >
-            <Item
+            >
+              <Grid xs={12}
+              style={{
+                marginTop: '-300px'
+              }}
+              >
+              <Item
               style={{
                 fontSize: "45px",
                 fontFamily: "monospace",
                 display: "flex",
                 justifyContent: "flex-start",
+                alignContent: 'flex-start'
               }}
             >
               Blusas
             </Item>
+              </Grid>
+              
+              
+            
+            </ImageButton>
+            
           </Grid>
           <Grid
             xs={5.7}
             style={{
-              backgroundImage: `url(https://visteconclase.com/imagenes/Puedo-combinar-un-abrigo-beige-de-hombre-con-zapatillas-deportivas.jpg)`,
-              height: "400px",
-              margin: 10,
-              borderRadius: "25px",
+              // backgroundImage: `url(https://visteconclase.com/imagenes/Puedo-combinar-un-abrigo-beige-de-hombre-con-zapatillas-deportivas.jpg)`,
+              // height: "400px",
+              marginLeft: 10,
+              // borderRadius: "25px",
 
               //  backgroundSize:'cover',
               //  backgroundRepeat: 'no-repeat',
@@ -210,7 +331,38 @@ const Landing = () => {
               //  backgroundAttachment: 'fixed'
             }}
           >
-            <Item
+            
+            <ImageButton
+            style={{
+              backgroundImage: `url(https://visteconclase.com/imagenes/Puedo-combinar-un-abrigo-beige-de-hombre-con-zapatillas-deportivas.jpg)`,
+              height: "400px",
+              width: '100%',
+              borderRadius: "25px",
+              backgroundSize: "cover",
+              backgroundRepeat: "no-repeat",
+              // backgroundPosition: "center center",
+            }}
+            >
+              <Grid xs={12}
+              style={{
+                marginTop: '300px'
+              }}
+              >
+              <Item
+              style={{
+                fontSize: "45px",
+                fontFamily: "monospace",
+                display: "flex",
+                justifyContent: "flex-end",
+                alignContent: 'flex-end',
+                color: 'black',
+              }}
+            >
+              Abrigos
+            </Item>
+              </Grid>
+            </ImageButton>
+            {/* <Item
               style={{
                 height: "350px",
                 fontSize: "45px",
@@ -222,23 +374,53 @@ const Landing = () => {
               }}
             >
               Abrigos
-            </Item>
+            </Item> */}
           </Grid>
           <Grid
-            xs={12}
+            xs={11.6}
             style={{
-              backgroundImage: `url(https://media.istockphoto.com/id/857856490/photo/cheerful-black-guy-in-london.jpg?b=1&s=170667a&w=0&k=20&c=7w7Bbt-U7QjHCI5JfZHawd5ivO0PqSWoaU6IxD4Jw68=)`,
-              height: "375px",
-              margin: 10,
-              borderRadius: "25px",
+              // backgroundImage: `url(https://media.istockphoto.com/id/857856490/photo/cheerful-black-guy-in-london.jpg?b=1&s=170667a&w=0&k=20&c=7w7Bbt-U7QjHCI5JfZHawd5ivO0PqSWoaU6IxD4Jw68=)`,
+              // height: "375px",
+              marginLeft: 10,
+              // borderRadius: "25px",
 
-              backgroundSize: "cover",
-              backgroundRepeat: "no-repeat",
-              backgroundPosition: "center center",
+              // backgroundSize: "cover",
+              // backgroundRepeat: "no-repeat",
+              // backgroundPosition: "center center",
               //  backgroundAttachment: 'fixed'
             }}
           >
-            <Item
+            <ImageButton
+            style={{
+              backgroundImage: `url(https://media.istockphoto.com/id/857856490/photo/cheerful-black-guy-in-london.jpg?b=1&s=170667a&w=0&k=20&c=7w7Bbt-U7QjHCI5JfZHawd5ivO0PqSWoaU6IxD4Jw68=)`,
+              height: "375px",
+              width: '100%',
+              borderRadius: "25px",
+              backgroundSize: "cover",
+              backgroundRepeat: "no-repeat",
+              backgroundPosition: "center center",
+            }}
+            >
+              <Grid xs={12}
+              style={{
+                marginTop: '-300px'
+              }}
+              >
+              <Item
+              style={{
+                fontSize: "60px",
+                fontFamily: "monospace",
+                display: "flex",
+                justifyContent: "flex-end",
+                alignContent: 'flex-end',
+                color: 'white',
+              }}
+            >
+              Remeras
+            </Item>
+              </Grid>
+            </ImageButton>
+            {/* <Item
               style={{
                 fontSize: "60px",
                 fontFamily: "monospace",
@@ -248,28 +430,57 @@ const Landing = () => {
               }}
             >
               Remeras
-            </Item>
+            </Item> */}
           </Grid>
         </Grid>
 
         <Grid
           xs={4}
           style={{
-            backgroundImage: `url(https://www.outfit-styles.com/wp-content/uploads/2019/07/Skinny-Jeans-Outfit-2019.jpg)`,
-            height: "800px",
-            marginTop: 10,
-            marginLeft: 10,
-            marginBottom: 10,
+            // backgroundImage: `url(https://www.outfit-styles.com/wp-content/uploads/2019/07/Skinny-Jeans-Outfit-2019.jpg)`,
+            // height: "800px",
+            // marginTop: 10,
+            // marginLeft: 10,
+            // marginBottom: 10,
 
             // borderRadius: '25px',
 
-            backgroundSize: "cover",
-            backgroundRepeat: "no-repeat",
+            // backgroundSize: "cover",
+            // backgroundRepeat: "no-repeat",
             //  backgroundPosition: 'center center',
             //  backgroundAttachment: 'fixed'
           }}
         >
-          <Item
+          <ImageButton
+           style={{
+            backgroundImage: `url(https://www.outfit-styles.com/wp-content/uploads/2019/07/Skinny-Jeans-Outfit-2019.jpg)`,
+            height: "800px",
+            width: '100%',
+            borderRadius: "25px",
+            backgroundSize: "cover",
+            backgroundRepeat: "no-repeat",
+            // backgroundPosition: "center center",
+          }}>
+            <Grid xs={12}
+              style={{
+                marginTop: '-700px'
+              }}
+              >
+              <Item
+              style={{
+                fontSize: "65px",
+                fontFamily: "monospace",
+                display: "flex",
+                justifyContent: "flex-start",
+                alignContent: 'flex-end',
+                color: 'black',
+              }}
+            >
+              Jeans
+            </Item>
+              </Grid>
+          </ImageButton>
+          {/* <Item
             style={{
               // height: '350px',
               fontSize: "65px",
@@ -280,7 +491,7 @@ const Landing = () => {
             }}
           >
             Jeans
-          </Item>
+          </Item> */}
         </Grid>
 
         <Grid
@@ -398,19 +609,50 @@ const Landing = () => {
         <Grid
           xs={5.7}
           style={{
-            backgroundImage: `url(https://i.pinimg.com/originals/18/34/99/183499d64bef2652a041e55bdd3fd7aa.jpg)`,
-            height: "600px",
+            // backgroundImage: `url(https://i.pinimg.com/originals/18/34/99/183499d64bef2652a041e55bdd3fd7aa.jpg)`,
+            // height: "600px",
             marginLeft: 20,
             marginTop: 20,
-            marginBottom: 20,
+            // marginBottom: 20,
+            // borderRadius: "25px",
+            // backgroundSize: "cover",
+            // backgroundRepeat: "no-repeat",
+            // backgroundPosition: "center center",
+            //  backgroundAttachment: 'fixed'
+          }}
+        >
+          <ImageButton
+          style={{
+            backgroundImage: `url(https://i.pinimg.com/originals/18/34/99/183499d64bef2652a041e55bdd3fd7aa.jpg)`,
+            height: "600px",
+            width: '100%',
             borderRadius: "25px",
             backgroundSize: "cover",
             backgroundRepeat: "no-repeat",
             backgroundPosition: "center center",
-            //  backgroundAttachment: 'fixed'
-          }}
-        >
-          <Item
+          }}>
+          
+          <Grid xs={12}
+              style={{
+                marginTop: '-460px'
+              }}
+              >
+              <Item
+              style={{
+                fontSize: "65px",
+                fontFamily: "monospace",
+                display: "flex",
+                justifyContent: "flex-start",
+                alignContent: 'flex-end',
+                color: 'white',
+              }}
+            >
+              Vestidos
+            </Item>
+              </Grid>
+
+          </ImageButton>
+          {/* <Item
             style={{
               // height: '350px',
               fontSize: "65px",
@@ -421,24 +663,55 @@ const Landing = () => {
             }}
           >
             Vestidos
-          </Item>
+          </Item> */}
         </Grid>
         <Grid
           xs={5.8}
           style={{
-            backgroundImage: `url(https://i.pinimg.com/originals/dc/53/94/dc5394110c2f8ce33f9fc13dc36d8abe.jpg)`,
-            height: "600px",
+            // backgroundImage: `url(https://i.pinimg.com/originals/dc/53/94/dc5394110c2f8ce33f9fc13dc36d8abe.jpg)`,
+            // height: "600px",
             marginLeft: 20,
             marginTop: 20,
-            marginBottom: 20,
+            // marginBottom: 20,
+            // borderRadius: "25px",
+            // backgroundSize: "cover",
+            // backgroundRepeat: "no-repeat",
+            // backgroundPosition: "center center",
+            //  backgroundAttachment: 'fixed'
+          }}
+        >
+          <ImageButton
+          style={{
+            backgroundImage: `url(https://i.pinimg.com/originals/dc/53/94/dc5394110c2f8ce33f9fc13dc36d8abe.jpg)`,
+            height: "600px",
+            width: '100%',
             borderRadius: "25px",
             backgroundSize: "cover",
             backgroundRepeat: "no-repeat",
             backgroundPosition: "center center",
-            //  backgroundAttachment: 'fixed'
           }}
-        >
-          <Item
+          >
+            <Grid xs={12}
+              style={{
+                marginTop: '460px'
+              }}
+              >
+              <Item
+              style={{
+                fontSize: "65px",
+                fontFamily: "monospace",
+                display: "flex",
+                justifyContent: "flex-start",
+                alignContent: 'flex-end',
+                color: 'white',
+              }}
+            >
+              Shorts
+            </Item>
+              </Grid>
+
+          </ImageButton>
+          {/* <Item
             style={{
               height: "100%",
               fontSize: "65px",
@@ -450,7 +723,7 @@ const Landing = () => {
             }}
           >
             Shorts
-          </Item>
+          </Item> */}
         </Grid>
         <Grid
           container
