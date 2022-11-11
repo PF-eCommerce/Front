b84dde1733947fcd9c8da6e955c80997562aeec1
sergio <sheko3857@gmail.com>
2022-11-10 23:20:55 -0800

    se renderiza carts desde back

--- ecommerce-front/src/pages/Landing/Landing.jsx
+++ ecommerce-front/src/pages/Landing/Landing.jsx
@@ -1,18 +1,11 @@
 import React from "react";
-import { useEffect } from "react";
-import {useDispatch} from "react-redux"
-import {getAllProducts} from "../../redux/actions/productsAction"
 
 
 
 
   const Landing = () => {
   
-  const dispatch = useDispatch()
-
-  useEffect(()=>{
-  dispatch(getAllProducts())
-  },[])
+  
      return <div>Landing</div>
 
       };
