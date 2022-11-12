f8b6b0ebc4b9fb79e045cff9c199d2d981624bee
ednesor <edgar.funes2001@gmail.com>
2022-11-11 21:01:51 -0300

    Se creo pagina de preguntas

---
+++ ecommerce-front/src/pages/Preguntas/Preguntas.jsx
@@ -0,0 +1,12 @@
+import { Box } from "@mui/material";
+import React from "react";
+
+const Preguntas = () => {
+    return(
+        <Box>
+            Preguntas
+        </Box>
+    )
+}
+
+export default Preguntas;
