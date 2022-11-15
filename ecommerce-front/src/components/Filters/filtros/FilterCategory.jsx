bcbe9b8c8f2ee0748532c4bdcef4648cc3e7e2e1
Angel Scutari <antonioscutari2@hotmail.com>
2022-11-15 16:26:12 -0300

    Arreglado el import

--- ecommerce-front/src/components/Filters/filtros/FilterCategory.jsx
+++ ecommerce-front/src/components/Filters/filtros/FilterCategory.jsx
@@ -1,55 +1,48 @@
-import * as React from 'react';
-import InputLabel from '@mui/material/InputLabel';
-import MenuItem from '@mui/material/MenuItem';
-import FormControl from '@mui/material/FormControl';
-import Select from '@mui/material/Select';
-import cates from "../../../utils/data/categories"
-import { useDispatch } from 'react-redux';
-import {getCategories} from "../../../redux/actions/productsAction"
+import * as React from "react";
+import InputLabel from "@mui/material/InputLabel";
+import MenuItem from "@mui/material/MenuItem";
+import FormControl from "@mui/material/FormControl";
+import Select from "@mui/material/Select";
+import { cates } from "../../../utils/data/categories";
+import { useDispatch } from "react-redux";
+import { getCategories } from "../../../redux/actions/productsAction";
 
 export default function SelectVariants() {
-  const [category, setCategory] = React.useState('');
+  const [category, setCategory] = React.useState("");
 
-  const dispatch = useDispatch()
- 
+  const dispatch = useDispatch();
 
   const handleChange = (event) => {
-    event.preventDefault()
-    const value = event.target.value
+    event.preventDefault();
+    const value = event.target.value;
     setCategory(dispatch(getCategories(value)));
- 
   };
 
-  
+  const categorias = cates;
 
-
-  const categorias = cates
- 
   return (
     <div>
-       
-      <FormControl variant="standard" sx={{ m: 1, minWidth: 120 }}>
-        <InputLabel id="demo-simple-select-standard-label">Categoria</InputLabel>
+      <FormControl variant='standard' sx={{ m: 1, minWidth: 120 }}>
+        <InputLabel id='demo-simple-select-standard-label'>
+          Categoria
+        </InputLabel>
         <Select
-          labelId="demo-simple-select-standard-label"
-          id="demo-simple-select-standard"
+          labelId='demo-simple-select-standard-label'
+          id='demo-simple-select-standard'
           value={category}
-          onChange={(e)=>handleChange(e)}
-          label="Categoria"
-         
+          onChange={(e) => handleChange(e)}
+          label='Categoria'
         >
-          <MenuItem value="">
+          <MenuItem value=''>
             <em>None</em>
           </MenuItem>
-          {
-            categorias?.map(categoria=>{
-              return(
-                <MenuItem value={categoria.valor}>{categoria.valor}</MenuItem>
-              )
-            })
-          }
+          {categorias?.map((categoria) => {
+            return (
+              <MenuItem value={categoria.valor}>{categoria.valor}</MenuItem>
+            );
+          })}
         </Select>
       </FormControl>
-   </div>
+    </div>
   );
 }
