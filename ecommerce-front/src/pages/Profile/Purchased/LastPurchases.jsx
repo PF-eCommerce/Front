f95fa20f1f25a5e458252adc880d96cafbcd94f8
Angel Scutari <antonioscutari2@hotmail.com>
2022-11-21 07:09:01 -0300

    Componente de últimas compras

---
+++ ecommerce-front/src/pages/Profile/Purchased/LastPurchases.jsx
@@ -0,0 +1,27 @@
+import React from "react";
+import Card from "./Card/Card";
+import { obj as purchases } from "../../../utils/data/productDetail";
+import { Stack } from "@mui/system";
+
+const LastPurchases = () => {
+  /*
+    const purchases = useSelector(state => state.orders)
+    */
+  const lastPurchases =
+    purchases.length > 4 ? purchases.slice(0, 3) : purchases;
+  return (
+    <Stack direction='row' spacing={1}>
+      {lastPurchases?.map((produc) => (
+        <Card
+          key={produc._id}
+          title={produc.title}
+          price={produc.price}
+          img={produc.img}
+          numStock={produc.numStock}
+        />
+      ))}
+    </Stack>
+  );
+};
+
+export default LastPurchases;
