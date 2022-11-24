4ed33e327989d8e5a5e8404c7ef6065148c7ea8d
Angel Scutari <antonioscutari2@hotmail.com>
2022-11-24 15:33:01 -0300

    Corregido error de merge y conectado a reducer

--- ecommerce-front/src/pages/Profile/Purchased/LastPurchases.jsx
+++ ecommerce-front/src/pages/Profile/Purchased/LastPurchases.jsx
@@ -1,25 +1,22 @@
 import React from "react";
-import RecipeReviewCard from "../../../components/Cards/Card/Card";
-import { obj as purchases } from "../../../utils/data/productDetail";
 import { Stack } from "@mui/system";
 import RecipeReviewCard from "../../../components/Cards/Card/Card";
 
 const LastPurchases = () => {
-  /*
-    const purchases = useSelector(state => state.orders)
-    */
-  // const lastPurchases = purchases?.slice(0, 3)
+  const purchases = React.useSelector((state) => state.orders.purchased);
+
+  const lastPurchases = purchases?.slice(0, 3);
   return (
     <Stack direction='row' spacing={1}>
-    
-        <RecipeReviewCard
-          key={obj._id}
-          title={obj.title}
-          price={obj.price}
-          img={obj.img}
-          numStock={obj.numStock}
-        />
-    
+      <RecipeReviewCard
+        key={purchases.length > 3 ? lastPurchases._id : purchases._id}
+        title={purchases.length > 3 ? lastPurchases.title : purchases.title}
+        price={purchases.length > 3 ? lastPurchases.price : purchases.price}
+        img={purchases.length > 3 ? lastPurchases.img : purchases.img}
+        numStock={
+          purchases.length > 3 ? lastPurchases.numStock : purchases.numStock
+        }
+      />
     </Stack>
   );
 };
