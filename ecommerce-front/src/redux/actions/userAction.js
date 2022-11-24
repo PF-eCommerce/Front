0db8949268f9a446997305f8d4b7dd0e73ce0cc0
Angel Scutari <antonioscutari2@hotmail.com>
2022-11-24 14:44:16 -0300

    Me colgué con éste

--- ecommerce-front/src/redux/actions/userAction.js
+++ ecommerce-front/src/redux/actions/userAction.js
@@ -12,7 +12,7 @@ export const ALL_USERS = "ALL_USERS";
 export const userRegister = (user) => {
   return async (dispatch) => {
     try {
-      await axios.post(`${process.env.REACT_APP_API_URL}/register/user`, user);
+      await axios.post(`/register/user`, user);
       return dispatch({
         type: USER_REGISTER,
       });
@@ -28,10 +28,7 @@ export const userRegister = (user) => {
 export const userLogin = (user) => {
   return async (dispatch) => {
     try {
-      const token = await axios.post(
-        `${process.env.REACT_APP_API_URL}/login/user`,
-        user
-      );
+      const token = await axios.post(`/login/user`, user);
 
       localStorage.setItem("token", token.data.token);
 
@@ -98,7 +95,7 @@ export const registerUserAuth0 = (user) => {
   console.log(user);
   return async () => {
     try {
-      await axios.post(`${process.env.REACT_APP_API_URL}/register/auth0`, user);
+      await axios.post(`/register/auth0`, user);
     } catch (error) {
       console.log(error);
     }
@@ -115,9 +112,7 @@ export const auth0User = (user) => {
 export const getAllUsers = () => {
   return async (dispatch) => {
     try {
-      const users = await axios.get(
-        `${process.env.REACT_APP_API_URL}/user/all`
-      );
+      const users = await axios.get(`/user/all`);
       return dispatch({
         type: ALL_USERS,
         payload: users.data,
