0baf4b37fcc2187a3b58b0619387e7017e4069be
Angel Scutari <antonioscutari2@hotmail.com>
2022-11-29 20:20:08 -0300

    Modificación inocua

--- ecommerce-front/src/pages/Preguntas/Preguntas.jsx
+++ ecommerce-front/src/pages/Preguntas/Preguntas.jsx
@@ -1,43 +1,55 @@
-import { Accordion, AccordionDetails, AccordionSummary, Box, Typography } from "@mui/material";
-import ExpandMoreIcon from '@mui/icons-material/ExpandMore';
+import {
+  Accordion,
+  AccordionDetails,
+  AccordionSummary,
+  Box,
+  Typography,
+} from "@mui/material";
+import ExpandMoreIcon from "@mui/icons-material/ExpandMore";
 import React from "react";
 import lista from "./lista";
 
 const Preguntas = () => {
-
-    const preguntas = () => {
-        return (
-            lista.map((p, i) => {
-                return (
-                    <Accordion key={i} sx={{ width: { xs: "100%", sm: "80%" } }}>
-                        <AccordionSummary
-                            expandIcon={<ExpandMoreIcon />}
-                            aria-controls="panel1a-content"
-                            id="panel1a-header"
-                        >
-                            <Typography>{p.pregunta}</Typography>
-                        </AccordionSummary>
-                        <AccordionDetails>
-                            <Typography>{p.respuesta}</Typography>
-                        </AccordionDetails>
-                    </Accordion>
-                )
-            })
-        )
-    }
-    return (
-        <Box sx={{
-            display: "flex",
-            flexWrap: "wrap",
-            justifyContent: "center"
+  const preguntas = () => {
+    return lista.map((p, i) => {
+      return (
+        <Accordion key={i} sx={{ width: { xs: "100%", sm: "80%" } }}>
+          <AccordionSummary
+            expandIcon={<ExpandMoreIcon />}
+            aria-controls='panel1a-content'
+            id='panel1a-header'
+          >
+            <Typography>{p.pregunta}</Typography>
+          </AccordionSummary>
+          <AccordionDetails>
+            <Typography>{p.respuesta}</Typography>
+          </AccordionDetails>
+        </Accordion>
+      );
+    });
+  };
+  return (
+    <Box
+      sx={{
+        display: "flex",
+        flexWrap: "wrap",
+        justifyContent: "center",
+      }}
+    >
+      <Typography
+        variant='h3'
+        sx={{
+          width: "100%",
+          textAlign: "center",
+          marginTop: 3,
+          marginBottom: 4,
         }}
-        >
-            <Typography variant="h3" sx={{ width: "100%", textAlign: "center", marginTop: 3, marginBottom: 4 }}>
-                Preguntas y respuestas
-            </Typography>
-            {preguntas()}
-        </Box>
-    )
-}
+      >
+        Preguntas frecuentes
+      </Typography>
+      {preguntas()}
+    </Box>
+  );
+};
 
 export default Preguntas;
