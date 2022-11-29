841f9e45ca0a4343ee35e397f8dbee1c41aad15f
Angel Scutari <antonioscutari2@hotmail.com>
2022-11-29 20:22:51 -0300

    Estilo

--- ecommerce-front/src/pages/Admin/Pages/Dashboard/Orders.jsx
+++ ecommerce-front/src/pages/Admin/Pages/Dashboard/Orders.jsx
@@ -4,50 +4,50 @@ import { useDispatch, useSelector } from "react-redux";
 import { Link } from "react-router-dom";
 import { getOrders } from "../../../../redux/actions/adminAction";
 
-const Container = styled('div')({
-    backgroundColor: "white",
-    width: "120px",
-    height: "100px",
-    borderRadius: "20px",
-    boxShadow: "0px 0px 5px #424242",
-    textDecoration: "none",
-    marginLeft:"10px",
-})
-const Title = styled('p')({
-    paddingLeft: "10px",
-    paddingTop: "10px",
-    fontSize: "15px",
-    fontWeight: "bold",
-    color: "gray",
-})
-const UserNum = styled('p')({
-    paddingLeft: "10px",
-    fontSize: "30px",
-    color:"#F44336"
-})
-const SubTitle = styled('p')({
-    paddingLeft: "10px",
-    fontSize:"14px",
-    textDecoration:"underline",
-    color:"#F44336"
-})
+const Container = styled("div")({
+  backgroundColor: "white",
+  width: "120px",
+  height: "100px",
+  borderRadius: "3px",
+  boxShadow: "0px 0px 1px #424242",
+  textDecoration: "none",
+  marginLeft: "10px",
+});
+const Title = styled("p")({
+  paddingLeft: "10px",
+  paddingTop: "10px",
+  fontSize: "15px",
+  fontWeight: "bold",
+  color: "gray",
+});
+const UserNum = styled("p")({
+  paddingLeft: "10px",
+  fontSize: "30px",
+  color: "#F44336",
+});
+const SubTitle = styled("p")({
+  paddingLeft: "10px",
+  fontSize: "14px",
+  textDecoration: "underline",
+  color: "#F44336",
+});
 
 export default function Users() {
-    const info = useSelector((state) => state.admin.orders)
+  const info = useSelector((state) => state.admin.orders);
 
-    const dispatch = useDispatch();
+  const dispatch = useDispatch();
 
-    useEffect(() => {
-        dispatch(getOrders())
-    }, [dispatch]);
+  useEffect(() => {
+    dispatch(getOrders());
+  }, [dispatch]);
 
-    return (
-        <Container>
-            <Link to={"/admin/orders"}>
-                <Title>Ordenes</Title>
-                <UserNum>{info.length}</UserNum>
-                <SubTitle>Ver todos</SubTitle>
-            </Link>
-        </Container>
-    )
+  return (
+    <Container>
+      <Link to={"/admin/orders"}>
+        <Title>Ordenes</Title>
+        <UserNum>{info.length}</UserNum>
+        <SubTitle>Ver todos</SubTitle>
+      </Link>
+    </Container>
+  );
 }
