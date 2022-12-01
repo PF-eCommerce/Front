caaca60b98e6dd09b1b80e42dbce68503c06a3bf
gcarreiras <gcarreiras86@gmail.com>
2022-12-01 09:27:03 -0300

    solucionado pequeño bug

--- ecommerce-front/src/pages/Profile/Reviews/ReviewsList.jsx
+++ ecommerce-front/src/pages/Profile/Reviews/ReviewsList.jsx
@@ -15,8 +15,8 @@ import { formatLine } from "../../../utils/functions";
 
 const ReviewsList = () => {
   const reviews = useSelector((state) => state.review.reviews);
-
-  if (reviews?.length > 0) {
+  setTimeout(() => {
+  if (reviews) {
     return (
       <Stack direction='row' spacing={1}>
         {reviews?.map((rev) => (
@@ -30,8 +30,6 @@ const ReviewsList = () => {
         height="280"
         image={img[0]}
         maxWidth="8"
-      
-        
       /> */}
             <CardContent>
               <Typography variant='body2' color='secondary'>
@@ -60,6 +58,7 @@ const ReviewsList = () => {
       </>
     );
   }
+ } , 500) 
 };
 
 export default ReviewsList;
